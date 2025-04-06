27c9cbb00f12330a080b9cccfa639e1a252a49a0
ybhas <ybhas@Bhaskar>
2025-04-06 11:19:13 +0530

    First commit

---
+++ src/test/java/Base/BaseTest.java
@@ -0,0 +1,59 @@
+package Base;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import Utilities.TestUtil;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BaseTest {
+
+	public static WebDriver driver;
+	public static Properties properties = new Properties();
+	public static FileReader fr;
+	
+	public BaseTest() {
+			FileReader fr;
+			try {
+				fr = new FileReader(System.getProperty("user.dir")+"/src/test/resources/configFile/Config.properties");
+				properties.load(fr);
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	}
+	
+	public void initialization() {
+
+		if (properties.getProperty("browser").equalsIgnoreCase("chrome")) {
+			WebDriverManager .chromedriver().setup();
+			driver=new ChromeDriver();
+			driver.get(properties.getProperty("URL"));
+		}
+		
+		else if (properties.getProperty("browser").equalsIgnoreCase("firefox")) {
+			WebDriverManager .firefoxdriver().setup();
+			driver=new ChromeDriver();
+			driver.get(properties.getProperty("URL"));
+		}
+		
+		else if (properties.getProperty("browser").equalsIgnoreCase("edge")) {
+			WebDriverManager .edgedriver().setup();
+			driver=new ChromeDriver();
+			driver.get(properties.getProperty("URL"));
+		}
+
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(TestUtil.implicitly_Wait));
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(TestUtil.pageLoadTimeOut));
+	}
+}
---
+++ src/test/java/Utilities/TestUtil.java
@@ -0,0 +1,6 @@
+package Utilities;
+
+public class TestUtil {
+	public static long implicitly_Wait = 10;
+	public static long pageLoadTimeOut = 23;
+}
